9317d58dbc48342da5927f5cf83991a9315c1959
Jacek Winiarczyk <kancelariajkw@gmail.com>
2024-03-25 18:00:08 +0100

    Comments for yamlgen

--- yamlgen.sh
+++ yamlgen.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-mkdir $1
+#run: ./yamlgen.sh P1
+mkdir $1 || (echo "Dir $1 already exists";exit -1)	#create dir named P1
 mkdir $1/yaml
 mkdir $1/png
 cd dataPoleski
-ls | while read f;
+ls | while read f;						#read files from dataPoleski
 do
-	t=`python3 ../t1script/t0.py $f`
-	file=../$1/yaml/$f.yaml
+	t=`python3 ../t1script/t0.py $f` 	#t_0
+	file=../$1/yaml/$f.yaml				#result .yaml file
+	#YAML file
 	echo "photometry_files:" > $file
 	echo "    dataPoleski/$f" >> $file
 	echo "starting_parameters:" >> $file
-	echo "    t_0: gauss 245$t 0.1" >> $file
+	echo "    t_0: gauss 245$t 0.1" >> $file	#short Julian to long Julian
 	echo "    u_0: uniform 0.001 1." >> $file
 	echo "    t_E: gauss 20. 5." >> $file
 	echo "min_values:" >> $file
