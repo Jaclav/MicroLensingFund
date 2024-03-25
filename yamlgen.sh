04276ac94009d640726085354861b547a441f4ad
Jacek Winiarczyk <kancelariajkw@gmail.com>
2024-03-25 16:25:06 +0100

    Yamlgen

--- yamlgen.sh
+++ yamlgen.sh
@@ -5,12 +5,12 @@ mkdir $1/png
 cd dataPoleski
 ls | while read f;
 do
-	t=`head $f -n 1 | awk -F ' ' '{print $1}'` # t_0
+	t=`python3 ../t1script/t0.py $f`
 	file=../$1/yaml/$f.yaml
 	echo "photometry_files:" > $file
-	echo "    $f" >> $file
+	echo "    dataPoleski/$f" >> $file
 	echo "starting_parameters:" >> $file
-	echo "    t_0: gauss $t 0.1" >> $file
+	echo "    t_0: gauss 245$t 0.1" >> $file
 	echo "    u_0: uniform 0.001 1." >> $file
 	echo "    t_E: gauss 20. 5." >> $file
 	echo "min_values:" >> $file
@@ -20,6 +20,5 @@ do
 	echo "    n_steps: 5000" >> $file
 	echo "plots:" >> $file
 	echo "    best model:" >> $file
-	echo "        file: ../$1/png/$f.png" >> $file
-	#python3 ../ulens_model_fit.py moje.yaml
+	echo "        file: $1/png/$f.png" >> $file
 done
