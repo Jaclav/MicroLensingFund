65c0742631500114fbac3f20bd049675af215f0c
Jacek Winiarczyk <kancelariajkw@gmail.com>
2024-03-25 21:41:20 +0100

    For Franciszek with love

--- scripts/super_mega_script.py
+++ scripts/super_mega_script.py
@@ -21,12 +21,15 @@ u = np.zeros(indeksy.size)
 for i in range(indeksy.size):
     j = indeksy[i]
     A[j] = 10 ** (-0.4 * (mag[j] - mag[indeksy[-5]]))
-    u[j] = np.sqrt(2) * np.sqrt(
-        1 / (-1 + A[j] ** 2)
-        - A[j] ** 2 / (-1 + A[j] ** 2)
-        + np.sqrt(A[j] ** 2 * (-1 + A[j] ** 2)) / (-1 + A[j] ** 2)
-    )
-    tE[i] = abs(time[indeksy[j]] - t0) / (np.sqrt(abs(u[i] ** 2 - u0**2)))
+    if u[j] == u0:
+        u[j] = u0
+    else:
+        u[j] = np.sqrt(2) * np.sqrt(
+            1 / (-1 + A[j] ** 2)
+            - A[j] ** 2 / (-1 + A[j] ** 2)
+            + np.sqrt(A[j] ** 2 * (-1 + A[j] ** 2)) / (-1 + A[j] ** 2)
+        )
+    tE[i] = abs(time[j] - t0) / (np.sqrt(abs(u[i] ** 2 - u0**2)))
 
 tEfinal = np.mean(tE)
 
