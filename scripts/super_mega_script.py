b5861462702758ff3f1df1e375c25b4267e3a087
Hansus00 <franciszek.hansdorfer@gmail.com>
2024-03-25 21:28:36 +0100

    fixed an astonishing mistake in super mega script

--- scripts/super_mega_script.py
+++ scripts/super_mega_script.py
@@ -3,7 +3,14 @@ import numpy as np
 
 (time,mag,r)=np.loadtxt(sys.argv[1], unpack=True)
 indeksy=np.argsort(mag)
-time[indeksy[5]]
+
+t0 = time[indeksy[5]]
+
+A0 = 10**(-0.4*(mag[indeksy[5]] - mag[indeksy[-5]]))
+
+u0 = np.sqrt(2) * np.sqrt(
+    1 / (-1 + A0**2) - A0**2 / (-1 + A0**2) + np.sqrt(A0**2 * (-1 + A0**2)) / (-1 + A0**2)
+)
 
 tE = np.zeros(indeksy.size)
 A = np.zeros(indeksy.size)
@@ -14,4 +21,8 @@ for i in range(indeksy.size):
     u[i] = np.sqrt(2) * np.sqrt(
     1 / (-1 + A[i]**2) - A[i]**2 / (-1 + A[i]**2) + np.sqrt(A[i]**2 * (-1 + A[i]**2)) / (-1 + A[i]**2)
     )
-    tE[i] = abs(time[indeksy[i]] - time[indeksy[5]]) / (np.sqrt(u[i]**2 - u[5]**2))
+    tE[i] = abs(time[indeksy[i]] - t0) / (np.sqrt(u[i]**2 - u0**2))
+
+tEfinal = np.mean(tE)
+
+print(t0,"  ",u0,"  ",tEfinal,"  ",A0)
