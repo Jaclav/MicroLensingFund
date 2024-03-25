aafb2668029faede156f358d6aa21f45da0e8659
Jacek Winiarczyk <kancelariajkw@gmail.com>
2024-03-25 22:26:46 +0100

    Dlaczego 103Mag

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,12 +1,4 @@
-echo "u0.py"
-echo "0.13 vs " `python3 ../scripts/u0.py OB03235_OGLE.txt`
-echo "0.55 vs " `python3 ../scripts/u0.py phot_ob08092_O4.dat`
-echo "0.375 vs " `python3 ../scripts/u0.py starBLG234.6.I.218982.dat`
-echo "t0.py"
-echo "2848.0 vs " `python3 ../scripts/t0.py OB03235_OGLE.txt`
-echo "5379.4 vs " `python3 ../scripts/t0.py phot_ob08092_O4.dat`
-echo "3628.3 vs " `python3 ../scripts/t0.py starBLG234.6.I.218982.dat`
-echo "tE.py"
-echo "61.5 vs " `python3 ../scripts/tE.py OB03235_OGLE.txt`
-echo "16 vs " `python3 ../scripts/tE.py phot_ob08092_O4.dat`
-echo "102 vs " `python3 ../scripts/tE.py starBLG234.6.I.218982.dat`
+echo t0 u0 tE A0
+python3 scripts/super_mega_script.py tests/OB03235_OGLE.txt
+python3 scripts/super_mega_script.py tests/phot_ob08092_O4.dat
+python3 scripts/super_mega_script.py tests/starBLG234.6.I.218982.dat
