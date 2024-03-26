99ab6f38ef265918f8d50834850655031462662e
lparda <luka.parda@gmail.com>
2024-03-26 13:10:39 +0100

    parallax

---
+++ yalmgenparallax.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import numpy as np
+
+# run: ./yamlgen.sh P1
+(name, ascension,declination)=np.loadtxt("Coords/coords.csv",unpack=True,delimiter=",",dtype=str)
+indeks = -1
+os.mkdir(sys.argv[1]+"/yaml2")
+os.chdir("dataPoleski")
+listFiles = os.listdir()
+for file in listFiles:
+    yamlN = "../" + sys.argv[1] + "/yaml2/" + file + ".yaml"
+    parN = "../" + sys.argv[1] + "/yaml/" + file + ".par"
+    print(parN)
+    (t0, u0, tE, A,tmin,tmax) = np.loadtxt(parN)
+
+    listF = os.listdir("../"+sys.argv[1]+"/yaml")
+    for f in listF:
+        if f[::5] == file[::5] and f[::-3] == ".OUT":
+            indeks += 1
+            with open( f"{f}" ) as in_file:
+
+                file = in_file
+
+            for line in file:
+                line.readline()
+                wyrazy = line.split()
+                if wyrazy[0] == "t_0":
+                    t0 = float(wyrazy[2])
+                elif wyrazy[0] == "u_0":
+                    u0 = float(wyrazy[2])
+                elif wyrazy[0] == "t_E":
+                    tE = float(wyrazy[2])
+    
+    yaml = open(yamlN, "w+")
+    yaml.writelines("photometry_files:\n")
+    yaml.writelines("    dataPoleski/" + file + "\n")
+    yaml.writelines("starting_parameters:\n")
+    yaml.writelines(
+        "    t_0: gauss 245" + str(t0) + "0 0.1\n"
+    )  # short Julian to long Julian
+    yaml.writelines(
+        "    u_0: gauss " + str(u0) + " " + str(0.3 * u0) + "\n"
+    )  # TODO: add uncertanity
+    yaml.writelines("    t_E: gauss " + str(tE) + " " + str(tE * 0.5) + "\n")
+    yaml.writelines("    pi_E_N: uniform " + str(-1.0) + " " + str(1.0) + "\n")
+    yaml.writelines("    pi_E_E: uniform " + str(-1.0) + " " + str(1.0) + "\n")
+    yaml.writelines("model:\n")
+    yaml.writelines("   coords: " + ascension[indeks] + " " + declination[indeks] + "\n")
+    yaml.writelines("min_values:\n")
+    yaml.writelines("    u_0: 0.\n")
+    yaml.writelines("    t_E: 0.\n")
+    yaml.writelines("fitting_parameters:\n")
+    yaml.writelines("    n_steps: 5000\n")
+    yaml.writelines("plots:\n")
+    yaml.writelines("    best model:\n")
+    yaml.writelines("        file: " + sys.argv[1] + "/png/" + file + ".png\n")
