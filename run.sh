fe71dfc048f0b63958d38b02af60d7f52ec2830e
Jacek Winiarczyk <kancelariajkw@gmail.com>
2024-03-27 01:36:05 +0100

    Runs scripts 3

--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-i=$1
-for f in sim_26_16_16/yaml/*.yaml
+for ((i=$1;i<=$2;i++))
 do
-  if ((i<$2))
-  then
-    i=$((i+1))
 	dataNum=$i
     if ((dataNum<10))
     then
@@ -13,5 +9,4 @@ do
     fi
 	echo $3/PAR-$dataNum-noaver.dat.yaml
     python3 ulens_model_fit.py $3/PAR-$dataNum-noaver.dat.yaml 1> $3/PAR-$dataNum-noaver.dat.OUT 2>$3/PAR-$dataNum-noaver.dat.ERR&
-  fi
 done
