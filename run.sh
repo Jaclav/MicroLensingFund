5dd65618596ea3a097a8072f5ee9ef482ce8b9ca
Jacek Winiarczyk <kancelariajkw@gmail.com>
2024-03-27 01:11:27 +0100

    Better run

--- run.sh
+++ run.sh
@@ -1,7 +1,25 @@
-#! /bin/tcsh
+#!/bin/bash
 
-foreach f ( $1/*.yaml )
-  ( python3 ulens_model_fit.py $f > $f:r.OUT ) >& $f:r.ERR &
-  sleep 3
-end
+i=$1
+for f in sim_26_16_16/yaml2/*.yaml
+do
+  if ((i<=$2))
+  then
+    echo $f
+    xi_P=$(($i%10))
+    if (($xi_P==0))
+    then
+      xi_P=10
+    fi
 
+    dataNum=$(($i/10+1))
+    if ((dataNum<10))
+    then
+    dataNum="0${dataNum}"
+    fi
+
+    echo sim_26_16_16/yaml2/PAR-$dataNum-noaver.dat.$xi_P.yaml
+    echo $i
+    i=$((i+1))
+  fi
+done
