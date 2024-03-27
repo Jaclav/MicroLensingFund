71c0b54c186a9ed19da009883338653163f9082d
Jacek Winiarczyk <kancelariajkw@gmail.com>
2024-03-27 11:39:53 +0100

    plots

--- yamlgen.py
+++ yamlgen.py
@@ -4,13 +4,13 @@ import numpy as np
 
 # run: python3 yamlgen.py simulation_date
 os.mkdir(sys.argv[1])
-os.mkdir(sys.argv[1] + "/yaml")
-os.mkdir(sys.argv[1] + "/png")
+os.mkdir(sys.argv[1] + "/nothing")
+os.mkdir(sys.argv[1] + "/nothing/png")
 os.chdir("dataPoleski")
 listFiles = os.listdir()
 for file in listFiles:
-    yamlN = "../" + sys.argv[1] + "/yaml/" + file + ".yaml"
-    parN = "../" + sys.argv[1] + "/yaml/" + file + ".par"
+    yamlN = "../" + sys.argv[1] + "/nothing/" + file + ".yaml"
+    parN = "../" + sys.argv[1] + "/nothing/" + file + ".par"
     parameters = os.system(
         "python3 ../scripts/super_mega_script.py " + file + " 2>/dev/null 1>" + parN
     )
@@ -30,5 +30,5 @@ for file in listFiles:
     yaml.writelines("    n_walkers: 12\n")
     yaml.writelines("plots:\n")
     yaml.writelines("    best model:\n")
-    yaml.writelines("        file: " + sys.argv[1] + "/png/" + file + ".png\n")
+    yaml.writelines("        file: " + sys.argv[1] + "/nothing/png/" + file + ".png\n")
     yaml.writelines("        time range: 245" + str(tmin) + " 245" + str(tmax) + "\n")
