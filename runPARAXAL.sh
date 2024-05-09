c30f9fe1cdc91527e94a97fc87474e64e8ab6dc6
Hansus00 <franciszek.hansdorfer@gmail.com>
2024-05-09 22:45:47 +0200

    minor mistake in runPARAXAL

--- runPARAXAL.sh
+++ runPARAXAL.sh
@@ -3,10 +3,10 @@
 for i in {5,6,15,39,57,58,59}
 do
   file=$1/PAR-$i-noaver.dat
-  echo $file.p.yaml
-  echo $file.m.yaml
+  echo $file+.yaml
+  echo $file-.yaml
 
-  python3 ulens_model_fit.py $file.p.yaml 1> $file.p.OUT 2>$file.p.ERR&
-  python3 ulens_model_fit.py $file.m.yaml 1> $file.m.OUT 2>$file.m.ERR&
+  python3 ulens_model_fit.py $file+.yaml 1> $file+.OUT 2>$file-.ERR&
+  python3 ulens_model_fit.py $file-.yaml 1> $file-.OUT 2>$file-.ERR&
   sleep 600
 done
