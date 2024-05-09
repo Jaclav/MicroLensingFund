28b8bb337689548706f88a574e6f57bff18133a3
Jacek Winiarczyk <kancelariajkw@gmail.com>
2024-05-09 14:37:23 +0200

    ch2

--- yamlgenparaxall.py
+++ yamlgenparaxall.py
@@ -15,93 +15,92 @@ indeks = -1
 os.mkdir("../" + sys.argv[1] + "/paraxall")
 os.mkdir("../" + sys.argv[1] + "/paraxall/png")
 
-(name, better, parallax, parallaxPath, xallarap, xallarapPath, deltaChi) = np.loadtxt(
-    "../" + sys.argv[1] + "/chi2.csv", unpack=True, delimiter=",", dtype=str
+(name, better, parallax, parallaxPath, xallarap, xallarapPaths, deltaChi) = np.loadtxt(
+    "../" + sys.argv[1] + "/chi2.csv", unpack=True, delimiter=",", dtype=str, skiprows=1
 )
-xallarapnames = []
 xallarapName = []
+xallarapPath = []
 for i in range(len(name)):
     if better[i] == "xallarap":
-        xallarapnames.append(xallarapPath[i])
+        xallarapPath.append(xallarapPaths[i])
         xallarapName.append(name[i])
 # katalog z xalarap
-
-for i in range(len(xallarapnames)):
-    with open(
-        "../" + sys.argv[1] + "/xallarap/" + xallarapName[i] + ".OUT", "r"
-    ) as fileOUT:
-        print("../" + sys.argv[1] + "/xallarap/" + xallarapnames[i] + ".OUT")
+for i in range(len(xallarapPath)):
+    with open(xallarapPath[i], "r") as fileOUT:
+        print(fileOUT.name)
         lines = fileOUT.readlines()
-        for line in lines:
-            float(line.split())
-            if "t_0" in line and "u_0":
-                t0 = line[0]
-                u0 = line[1]
-                tE = line[2]
-            elif line[0] =="xi_period":
-                xi_period = str(line)
+        for k in range(len(lines)):
+            if "t_0" in lines[k] and "u_0" in lines[k]:
+                keys = lines[k].split()
+                vals = lines[k + 1].split()
+                tab = {}
+                for j in range(len(keys)):
+                    tab[keys[j]] = vals[j]
+                t0 = float(tab["t_0"])
+                u0 = float(tab["u_0"])
+                tE = float(tab["t_E"])
+                xi_period = float(tab["xi_period"])
+                break
 
-    # tutaj sie zatrzymałem
-    
-        for sign in ["p", "m"]:
-            newFile = xallarapName[i] + "." + sign
-            yamlN = "../" + sys.argv[1] + "/paraxall/" + newFile + ".yaml"
-            yaml = open(yamlN, "w+")
-            t0par = round(t0, -1)
-            graphicF = sys.argv[1] + "/paraxall/png/" + newFile
-            # TODO: gauss do tego
-            # TODO: bez 10 przedziałów
-            YAML = [
-                "photometry_files:",
-                "    dataPoleski/" + xallarapName[i],
-                "starting_parameters:",
-                "    t_0: gauss 245" + str(t0) + " 0.1",
-                "    u_0: gauss " + sign + str(u0) + " " + str(0.3 * u0),
-                "    t_E: gauss " + str(tE) + " " + str(tE * 0.1),
-                # parallax piE=sqrt(PiN^2+pIEE^2)
-                "    pi_E_N: gauss 0.00 0.01",
-                "    pi_E_E: gauss 0.00 0.01",
-                # PARAXALL https://doi.org/10.3847/1538-3881/ad284f
-                "    xi_Omega_node: uniform -20 380",
-                "    xi_inclination: uniform -20 380",
-                xi_period,
-                "    xi_semimajor_axis: log-uniform 0.001 0.1",
-                "    xi_argument_of_latitude_reference: uniform -20 380",
-                # parallax
-                "model:",
-                "   coords: " + right_ascension[indeks] + " " + declination[indeks],
-                "fixed_parameters:",
-                "    t_0_par: 245" + str(t0par),
-                "    t_0_xi: 245" + str(t0par),
-                "min_values:",
-                ("    u_0: 0." if sign == "p" else ""),
-                "    t_E: 0.",
-                "    xi_semimajor_axis: 0.",
-                "    xi_period: 0.",
-                "    xi_Omega_node: -20.",
-                "    xi_inclination: -20.",
-                "    xi_argument_of_latitude_reference: -20.",
-                "    pi_E_N: -1.",
-                "    pi_E_E: -1.",
-                "max_values:",
-                "    xi_Omega_node: 380.",
-                "    xi_inclination: 380.",
-                "    xi_argument_of_latitude_reference: 380.",
-                ("    u_0: 0." if sign == "m" else ""),
-                "    pi_E_N: 1.",
-                "    pi_E_E: 1.",
-                "fitting_parameters:",
-                "    n_steps: 400",
-                "    n_walkers: 1000",
-                "plots:",
-                "    best model:",
-                "        file: " + graphicF + ".png",
-                "    trajectory:",
-                "        file: " + graphicF + ".trj.png",
-                "    triangle:",
-                "        file: " + graphicF + ".trg.png",
-                "    trace:",
-                "        file: " + graphicF + ".tra.png",
-            ]
-            for line in YAML:
-                yaml.writelines(line + "\n")
+    for sign in ["p", "m"]:
+        newFile = xallarapName[i] + "." + sign
+        yamlN = "../" + sys.argv[1] + "/paraxall/" + newFile + ".yaml"
+        yaml = open(yamlN, "w+")
+        t0par = round(t0, -1)
+        graphicF = sys.argv[1] + "/paraxall/png/" + newFile
+        # TODO: gauss do tego
+        # TODO: bez 10 przedziałów
+        YAML = [
+            "photometry_files:",
+            "    dataPoleski/" + xallarapName[i],
+            "starting_parameters:",
+            "    t_0: gauss 245" + str(t0) + " 0.1",
+            "    u_0: gauss " + sign + str(u0) + " " + str(0.3 * u0),
+            "    t_E: gauss " + str(tE) + " " + str(tE * 0.1),
+            # parallax piE=sqrt(PiN^2+pIEE^2)
+            "    pi_E_N: gauss 0.00 0.01",
+            "    pi_E_E: gauss 0.00 0.01",
+            # PARAXALL https://doi.org/10.3847/1538-3881/ad284f
+            "    xi_Omega_node: uniform -20 380",
+            "    xi_inclination: uniform -20 380",
+            xi_period,
+            "    xi_semimajor_axis: log-uniform 0.001 0.1",
+            "    xi_argument_of_latitude_reference: uniform -20 380",
+            # parallax
+            "model:",
+            "   coords: " + right_ascension[indeks] + " " + declination[indeks],
+            "fixed_parameters:",
+            "    t_0_par: 245" + str(t0par),
+            "    t_0_xi: 245" + str(t0par),
+            "min_values:",
+            ("    u_0: 0." if sign == "p" else ""),
+            "    t_E: 0.",
+            "    xi_semimajor_axis: 0.",
+            "    xi_period: 0.",
+            "    xi_Omega_node: -20.",
+            "    xi_inclination: -20.",
+            "    xi_argument_of_latitude_reference: -20.",
+            "    pi_E_N: -1.",
+            "    pi_E_E: -1.",
+            "max_values:",
+            "    xi_Omega_node: 380.",
+            "    xi_inclination: 380.",
+            "    xi_argument_of_latitude_reference: 380.",
+            ("    u_0: 0." if sign == "m" else ""),
+            "    pi_E_N: 1.",
+            "    pi_E_E: 1.",
+            "fitting_parameters:",
+            "    n_steps: 400",
+            "    n_walkers: 1000",
+            "plots:",
+            "    best model:",
+            "        file: " + graphicF + ".png",
+            "    trajectory:",
+            "        file: " + graphicF + ".trj.png",
+            "    triangle:",
+            "        file: " + graphicF + ".trg.png",
+            "    trace:",
+            "        file: " + graphicF + ".tra.png",
+        ]
+        for line in YAML:
+            yaml.writelines(str(line) + "\n")
