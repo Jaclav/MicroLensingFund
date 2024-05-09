54a3934f3e6d0bdceb330cc9c6db196feda01cae
Jacek Winiarczyk <kancelariajkw@gmail.com>
2024-05-09 22:42:39 +0200

    runner

--- runPARAXAL.sh
+++ runPARAXAL.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-#./run 0 9 SIM
-#PAR-01.1 -- PAR-01.10
-#./run 10 19 SIM
-#PAR-02.1 -- PAR-02.10
-
-for ((i=$1;i<=$2;i++))
+for i in {5,6,15,39,57,58,59}
 do
-
-  
-
-  file=$3/PAR-$i-noaver.dat
+  file=$1/PAR-$i-noaver.dat
   echo $file.p.yaml
   echo $file.m.yaml
 
   python3 ulens_model_fit.py $file.p.yaml 1> $file.p.OUT 2>$file.p.ERR&
   python3 ulens_model_fit.py $file.m.yaml 1> $file.m.OUT 2>$file.m.ERR&
-  if(($i%10==9))
-  then
-  sleep 1500
-  fi	
+  sleep 600
 done
