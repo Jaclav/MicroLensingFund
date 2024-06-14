a320ebf383a37203d29578f746614e6fa13a47b3
Franciszek Hansdorfer <franciszek.hansdorfer@gmail.com>
2024-06-14 13:45:47 +0200

    last bits of the presentation

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-for ((i=$1;i<=$2;i++))
-do
-dataNum=$i
-    if ((dataNum<10))
-    then
-    dataNum="0${dataNum}"
-    fi
-	echo $3/PAR-$dataNum-noaver.dat.yaml
-    python3 ulens_model_fit.py $3/PAR-$dataNum-noaver.dat.yaml 1> $3/PAR-$dataNum-noaver.dat.OUT 2>$3/PAR-$dataNum-noaver.dat.ERR&
-done
+
+
+  file=$1/PAR-01-noaver.dat
+
+
+  python3 ulens_model_fit.py $file.yaml 1> $file.OUT 2>$file.ERR&
